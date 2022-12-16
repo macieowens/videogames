f4305a6085aaf8f96c48d91b2f68c61fa8207e1c
macieowens <112588931+macieowens@users.noreply.github.com>
2022-12-16 12:32:06 -0600

    inital commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace videogames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = new List<string>() { "tetris", "call of duty", "animal crossing", "vikings of vahalla", "stardew valley", "minecraft" };
+
+            var sort = games.OrderBy(game => game);
+
+            games.Sort();
+
+            foreach(var game in sort)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
